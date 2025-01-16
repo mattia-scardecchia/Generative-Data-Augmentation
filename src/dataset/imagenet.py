d1bdf58a4c781ebb699cd54c92f68529f825bf6d
MattiaSC01 <mattia.scardecchia@studbocconi.it>
2025-01-16 12:31:19 +0100

    fix: handle val file structure for use with IageFolder in tiny-imagenet

--- src/dataset/imagenet.py
+++ src/dataset/imagenet.py
@@ -34,44 +34,62 @@ class TinyImageNetDataModule(BaseDataModule):
         """
         Directory reorg code from: https://github.com/pytorch/vision/issues/6127
         """
+        dst_path = os.path.join(self.config["data_dir"], "tiny-imagenet-200")
+        if os.path.exists(dst_path):
+            return
+
         path = kagglehub.dataset_download("akash2sharma/tiny-imagenet")
         src_path = os.path.join(path, "tiny-imagenet-200")
-        dst_path = os.path.join(self.config["data_dir"], "tiny-imagenet-200")
-        if not os.path.exists(dst_path):
-            logging.info(f"Moving {src_path} to {dst_path}")
-            os.rename(src_path, dst_path)
-            curr_wdir = os.getcwd()
-            os.chdir(dst_path)
+        logging.info(f"Moving {src_path} to {dst_path}")
+        os.rename(src_path, dst_path)
+        curr_wdir = os.getcwd()
+        os.chdir(dst_path)
 
-            logging.info("Reorganizing data to fit ImageFolder expected structure")
-            wordmap = {}
-            with open("words.txt") as words, open("wnids.txt") as wnids:
-                for line in wnids:
-                    vals = line.split()
-                    wordmap[vals[0]] = ""
-                for line in words:
-                    vals = line.split()
-                    if vals[0] in wordmap:
-                        single_words = vals[1:]
-                        classname = re.sub(",", "", single_words[0])
-                        if len(single_words) >= 2:
-                            classname += "_" + re.sub(",", "", single_words[1])
-                        wordmap[vals[0]] = classname
-                        create_dir("./val/images/", classname)
-                        if os.path.exists("./train/" + vals[0]):
-                            os.rename("./train/" + vals[0], "./train/" + classname)
-                        # create_dir('./test/images/', single_words[0])
-                        # create_dir('./train/images/', single_words[0])
+        logging.info("Reorganizing data to fit ImageFolder expected structure")
+        wordmap = {}
+        with open("words.txt") as words, open("wnids.txt") as wnids:
+            for line in wnids:
+                vals = line.split()
+                wordmap[vals[0]] = ""
+            for line in words:
+                vals = line.split()
+                if vals[0] in wordmap:
+                    single_words = vals[1:]
+                    classname = re.sub(",", "", single_words[0])
+                    if len(single_words) >= 2:
+                        classname += "_" + re.sub(",", "", single_words[1])
+                    wordmap[vals[0]] = classname
+                    create_dir("./val/images/", classname)
+                    if os.path.exists("./train/" + vals[0]):
+                        os.rename("./train/" + vals[0], "./train/" + classname)
+                    # create_dir('./test/images/', single_words[0])
+                    # create_dir('./train/images/', single_words[0])
 
-            reorg("val/val_annotations.txt", "val/images/", wordmap)
-            os.chdir(curr_wdir)
+        reorg("val/val_annotations.txt", "val/images/", wordmap)
+
+        # reorganize val images layout for consistency with train split
+        for folder in os.listdir("val/images/"):
+            assert os.path.isdir(os.path.join("val/images/", folder))
+            os.makedirs(f"val/{folder}/images", exist_ok=False)
+            os.rename(f"val/images/{folder}/", f"val/{folder}/images/")
+        assert len(os.listdir("val/images")) == 0
+        os.rmdir("val/images")
+
+        # kagglehub checks that path exists before downloading; clean up to allow re-download
+        # without manually cleaning cache
+        assert len(os.listdir(path)) == 0, (
+            f"Trying to delete folder at path {path}, but it is not empty!"
+        )
+        os.rmdir(path)
+
+        os.chdir(curr_wdir)
 
     def get_dataset(self, split: str, transform):
         if split == "test":
             logging.warning(
                 "No labels are available for the test split... Loading val dataset instead!"
             )
-            split = "val"  # this makes sense cause augmentations are handled differently for val and test
+            split = "val"
         return ImageFolder(
             os.path.join(self.config["data_dir"], "tiny-imagenet-200", split), transform
         )
