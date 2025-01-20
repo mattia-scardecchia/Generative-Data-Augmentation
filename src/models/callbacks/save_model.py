ae4803fc3bea7b3385a4a6450b301e5a4cc532f1
Willinki <d.badalotti@campus.unimib.it>
2025-01-20 11:59:02 +0100

    fix: using local file and deleting

--- src/models/callbacks/save_model.py
+++ src/models/callbacks/save_model.py
@@ -1,5 +1,5 @@
+import os
 import torch
-import io
 from typing import Dict, Optional, Any
 import pytorch_lightning as pl
 import logging
@@ -20,19 +20,18 @@ class SaveModelCallback(pl.Callback):
         self.name = artifact_name
         self.metadata = artifact_metadata
         self.description = description if description is not None else ""
-        self.trained_model_name = "trained_model.pth"
+        self.trained_model_path = "tmp_trained_model.pth"
 
     def on_train_end(self, trainer, pl_module):
-        logger.info(f"saving model state dict as artifact: {self.name}")
-        buffer = io.BytesIO()
-        torch.save(pl_module.state_dict(), buffer)
-        buffer.seek(0)
+        torch.save(pl_module.state_dict(), self.trained_model_path)
         artifact = wandb.Artifact(
             name=self.name,
             type="model",
             description=self.description,
             metadata=self.metadata,
         )
-        artifact.add_file(buffer, name=self.trained_model_name)
+        artifact.add_file(self.trained_model_path, name=self.name)
         wandb.log_artifact(artifact)
         logger.info(f"Uploaded model with metadata as artifact: {artifact.name}")
+        os.remove(self.trained_model_path)
+        logger.info(f"Local file correctly removed")
